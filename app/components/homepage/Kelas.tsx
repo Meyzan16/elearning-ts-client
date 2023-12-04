2bc61c7eef71ec3c9f3e721f638f782ccc49bd61
Meyzan16 <meyzan1605@gmail.com>
2023-12-04 08:56:17 +0700

    create fitur swiper react

--- app/components/homepage/Kelas.tsx
+++ app/components/homepage/Kelas.tsx
@@ -1,98 +1,109 @@
+"use client";
 import Image from "next/image";
-import React, { FC, useEffect, useRef, useState } from "react";
 import Data from "@/app/Data/Data";
-
-
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import { useMediaQuery } from "react-responsive";
 
 const Kelas = () => {
-  
+  const isLG = useMediaQuery({ minWidth: 1024, maxWidth: 1279 });
+  const isXL = useMediaQuery({ minWidth: 1280 });
+
+  // Set slidesPerView sesuai dengan lebar layar
+  const slidesPerView = isLG ? 3 : isXL ? 5 : 1;
 
   return (
-    <div className="mb-20 hidden sm:flex ">
-      <div className="w-full">
-        <div className="text-center">
-          <div className="text-primary font-Poppins text-lg font-semibold">
-            Start learning today
+    <div className="mb-20 hidden lg:flex ">
+      <div className=" w-full items-center justify-center ">
+        <div className="flex flex-col  gap-8 w-full justify-center items-center">
+          <div className="text-center">
+            <div className="text-primary font-Poppins text-lg font-semibold">
+              Mastering Freelancer Tools
+            </div>
+            <div className="text-3xl text-slate-700 font-Poppins pt-2 font-bold leading-relaxed ">
+              Kelas Online BuildRunLearning.
+              <span className="block mt-[2px]">Teknologi terbaru</span>
+            </div>
           </div>
-          <div className="text-3xl text-slate-700 font-Poppins pt-2 font-bold leading-relaxed ">
-            Kelas Online BuildRunLearning.
-            <span className="block mt-[2px]">Materi Paling Update.</span>
-          </div>
-        </div>
 
-        <div className="mt-12 ">
-          <div className="grid grid-cols-2 gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-5 ">
-            {
-              Data && Data.map((i,index) => (
-                <div key={index}>
-                  <div
-                    className="px-10 py-4 gap-8 bg-white flex items-center justify-center 
-                                              overflow-hidden shadow-lg 
-                                              shadow-slate-300 hover:scale-105 transition-all duration-500 rounded-2xl mb-4"
+          <Swiper
+            slidesPerView={slidesPerView}
+            loop={true}
+            autoplay={{
+              delay: 0,
+              disableOnInteraction: false,
+            }}
+            speed={4000}
+            modules={[Autoplay]}
+            className="w-full  "
+          >
+            {Data.slice(0, 6).map((i, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="py-4 bg-white flex justify-center items-center gap-4 overflow-hidden  hover:scale-105 transition-all duration-500 rounded-2xl"
+                  style={{ marginRight: "20px" }} // Tambahkan properti style dengan marginRight
+                >
+                  <figure className="">
+                    <Image
+                      src={require(`../../../public/assets/skills/${i.image}`)}
+                      alt={`${i.title}`}
+                      className="w-16"
+                    />
+                  </figure>
+                  <h5
+                    className="items-center justify-center text-left 
+                                  font-bold font-Poppins  text-medium text-lg  sm:text-xl text-stone-700 "
                   >
-                    <figure className="flex items-center justify-center">
-                      <Image
-                        src={require(`../../../public/assets/${i.image}`)}
-                        alt={`${i.title}`}
-                        className="w-16"
-                      />
-                    </figure>
-
-                    <h5
-                      className="items-center justify-center text-left 
-                                              font-bold font-Poppins  text-medium text-lg  sm:text-xl text-stone-700 "
-                    >
-                      {i.title}
-                      <p className="font-Poppins text-slate-400 text-sm font-[46] pt-1">
-                        {i.subTitle}
-                      </p>
-                    </h5>
-                  </div>
+                    {i.title}
+                    <p className="font-Poppins text-slate-400 text-sm font-[46] pt-1">
+                      {i.subTitle}
+                    </p>
+                  </h5>
                 </div>
-              ))
-            }
-          </div>
-        </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
 
-       <div className="mt-4 ">
-          <div className="grid grid-cols-2 gap-4 md:gap-8 lg:grid-cols-5 ">
-            {
-              Data && Data.map((i,index) => (
-                <div key={index}>
-                  <div
-                    className="banner-item  px-10 py-4 gap-8 bg-white flex items-center justify-center 
-                                              overflow-hidden shadow-lg 
-                                              shadow-slate-300 hover:scale-105 transition-all duration-500 rounded-2xl mb-4"
+          <Swiper
+            slidesPerView={slidesPerView}
+            loop={true}
+            autoplay={{
+              delay: 0,
+              disableOnInteraction: false,
+              reverseDirection: true,
+            }}
+            speed={4000}
+            modules={[Autoplay]}
+            className="w-full"
+          >
+            {Data.slice(6, 12).map((i, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="py-4 bg-white flex justify-center items-center gap-4 overflow-hidden  hover:scale-105 transition-all duration-500 rounded-2xl"
+                  style={{ marginRight: "20px" }} // Tambahkan properti style dengan marginRight
+                >
+                  <figure className="">
+                    <Image
+                      src={require(`../../../public/assets/skills/${i.image}`)}
+                      alt={`${i.title}`}
+                      className="w-16"
+                    />
+                  </figure>
+                  <h5
+                    className="items-center justify-center text-left 
+                                  font-bold font-Poppins  text-medium text-lg  sm:text-xl text-stone-700 "
                   >
-                    <figure className="flex items-center justify-center">
-                      <Image
-                        src={require(`../../../public/assets/${i.image}`)}
-                        alt={`${i.title}`}
-                        className="w-16"
-                      />
-                    </figure>
-
-                    <h5
-                      className="items-center justify-center text-left 
-                                              font-bold font-Poppins  text-medium text-lg  sm:text-xl text-stone-700 "
-                    >
-                      {i.title}
-                      <p className="font-Poppins text-slate-400 text-sm font-[46] pt-1">
-                        {i.subTitle}
-                      </p>
-                    </h5>
-                  </div>
+                    {i.title}
+                    <p className="font-Poppins text-slate-400 text-sm font-[46] pt-1">
+                      {i.subTitle}
+                    </p>
+                  </h5>
                 </div>
-
-              ))
-            }
-            
-           
-
-          </div>
-        </div> 
-
-
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
